5014eda60823a18711ea5b371a4891695ccc4b5b
gangotrinvents <gangotrinvents@gmail.com>
2026-01-21 19:42:45 +0530

    5 brk Achieved Modularity

---
+++ Testing_Module.py
@@ -0,0 +1,9 @@
+## Here are going to import our class "brk"
+## we are importing class "brk" from file "Social_Media"
+
+from Social_Media import brk
+
+brk = brk()
+
+## As we are able to create module "Social_Media" and able to import class from that module
+## This is called "Modularity"
